a518c9d3be15231b09c14082c0d4850f6ec04a76
Satyam Kumar Mishra <139174123+SatyamkMishra@users.noreply.github.com>
2024-08-22 12:23:18 +0530

    Add files via upload

---
+++ JavaScript/variable.js
@@ -0,0 +1,28 @@
+var a = 3;
+console.log("the value of a is " + a);
+
+var a = 5;
+console.log("the value of a is " + a);
+
+var a = "Satyam"
+console.log("the value of a is " + a);
+
+//that's the problem in var and it is used before 2015 after that let and const conmes into the picture
+
+
+let b = 12;
+console.log("the value of b is " + b);
+
+b = "Satyam";
+console.log("the value of b is " + b);
+
+
+//but we cant decleare again let b = 12 it gives error
+
+
+const t = 23;
+console.log("the value of t is " + t );
+
+// t = "24";
+// console.log("the value of t is " + t );
+///it gives error
