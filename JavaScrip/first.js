98c2b5c41e5262940d6a7bf628c15b296bf10945
Satyam Kumar Mishra <139174123+SatyamkMishra@users.noreply.github.com>
2024-08-22 12:21:40 +0530

    Add files via upload

---
+++ JavaScrip/first.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+console.log("Helo Js");
+
+console.log("Lets begin");
+
+a = 19;
+console.log(a);
+
+fname = "Satyam";
+console.log(fname);
+
+b = 1.9;
+console.log(b);
+
+c = false;
+console.log(c);
+
+f = null;
+console.log(f);
